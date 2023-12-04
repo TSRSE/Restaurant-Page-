998b05a3f1a700bea80ab26972e64f1602f3e500
TSRSE <26186238+TSRSE@users.noreply.github.com>
2023-12-04 19:16:32 +0300

    added new js component boilerplates

---
+++ src/button.js
@@ -0,0 +1,22 @@
+function createButton(size, content){
+    let newButton = document.createElement('button')
+    switch (size) {
+        case big:
+            newButton.classList.add('big')
+            break;
+        case medium:
+            newButton.classList.add('medium')
+            break;
+        case small:
+            newButton.classList.add('small')
+            break;
+    
+        default:
+            newButton.classList.add('medium')
+            break;
+    }
+    newButton.innerText = content;
+    return newButton
+}
+
+export default createButton
